c695e2b9860f307afd1bd7efc3dc84347c095efc
Anatoly Polivin <tolydefault@gmail.com>
2023-08-06 22:50:22 +0400

    feat(terraform): open ports for ssh and wireguard

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,6 +19,22 @@ resource "aws_lightsail_instance" "wireguard" {
   }
 }
 
+resource "aws_lightsail_instance_public_ports" "test" {
+  instance_name = aws_lightsail_instance.wireguard.name
+
+  port_info {
+    protocol  = "udp"
+    from_port = 51820
+    to_port   = 51820
+  }
+
+  port_info {
+    protocol  = "tcp"
+    from_port = 22
+    to_port   = 22
+  }
+}
+
 resource "local_file" "ansible_inventory" {
   content  = "lightsail ansible_user=admin ansible_host=${aws_lightsail_instance.wireguard.public_ip_address} ansible_ssh_private_key_file=../id_rsa"
   filename = "${path.module}/../ansible/hosts"
